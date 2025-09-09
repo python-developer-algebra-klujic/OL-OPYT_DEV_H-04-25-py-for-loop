565fa5edad9a797c633632e93c50a3cce617596d
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-09 18:44:15 +0200

    Update for_loop_02.py

--- for_loop_02.py
+++ for_loop_02.py
@@ -1,2 +1,62 @@
-# Napisite program koji ispisuje trokut tako da su linije stranica prikazane zvjezdicama *.
+'''
+Napisite program koji ispisuje trokut tako da su linije stranica prikazane zvjezdicama *.
 
+*
+**
+* *
+*  *
+*   *
+*    *
+*******
+'''
+print('Ispis trokuta fiksne visine bez for petlje')
+print('*')
+print('**')
+print('* *')
+print('*  *')
+print('*   *')
+print('*    *')
+print('*******')
+print()
+print()
+
+
+
+
+
+
+
+
+
+print('Ispis trokuta fiksne visine pomocu for petlje')
+print('*')
+
+for number in range(5):
+    print('*' + ' ' * number + '*')
+
+print('*******')
+print()
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+print('Ispis trokuta varijabilne visine pomocu for petlje')
+
+height = int(input('Unesite visinu trokuta: '))
+
+print('*')
+for number in range(height):
+    print('*' + ' ' * number + '*')
+
+print('*' + '*' * height + '*')
+print()
+print()
