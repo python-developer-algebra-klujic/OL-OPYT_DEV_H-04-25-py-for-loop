db24ac1b938d4665469ba1578794faf558826934
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-09 20:02:34 +0200

    Update for_loop_04.py

--- for_loop_04.py
+++ for_loop_04.py
@@ -9,3 +9,18 @@ Napisite program koji ispisuje trokut tako da su linije stranica prikazane zvjez
  *    *
 *******
 '''
+# Denis Šragalj: 
+
+# pitajmo korisnika koji znak želi koristiti
+hight = int(input("Unesite visinu trokuta: " ))
+symbol = str(input('Unesite znak koji želite koristiti za ispis trokuta: ')  )
+
+
+print(f'{(hight - 1) * ' '}{symbol}')
+
+for number in range(hight - 2):
+    space_before = hight - 2 - number
+    print(f"{space_before * ' '}{symbol}{' ' * number}{symbol}")
+
+
+print(f'{symbol * hight}') 
