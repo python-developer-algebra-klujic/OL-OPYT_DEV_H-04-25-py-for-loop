a08ae1733d16b212ec0a81fc72a7979bc39ab539
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-09 19:42:21 +0200

    Update for_loop_02.py

--- for_loop_02.py
+++ for_loop_02.py
@@ -1,26 +1,24 @@
-'''
-Napisite program koji ispisuje trokut tako da su linije stranica prikazane zvjezdicama *.
+# '''
+# Napisite program koji ispisuje trokut tako da su linije stranica prikazane zvjezdicama *.
 
-*
-**
-* *
-*  *
-*   *
-*    *
-*******
-'''
-print('Ispis trokuta fiksne visine bez for petlje')
-print('*')
-print('**')
-print('* *')
-print('*  *')
-print('*   *')
-print('*    *')
-print('*******')
-print()
-print()
-
-
+# *
+# **
+# * *
+# *  *
+# *   *
+# *    *
+# *******
+# '''
+# print('Ispis trokuta fiksne visine bez for petlje')
+# print('*')
+# print('**')
+# print('* *')
+# print('*  *')
+# print('*   *')
+# print('*    *')
+# print('*******')
+# print()
+# print()
 
 
 
@@ -28,17 +26,17 @@ print()
 
 
 
-print('Ispis trokuta fiksne visine pomocu for petlje')
-print('*')
-
-for number in range(5):
-    print('*' + ' ' * number + '*')
-
-print('*******')
-print()
-print()
 
 
+# print('Ispis trokuta fiksne visine pomocu for petlje')
+# print('*')
+
+# for number in range(5):
+#     print('*' + ' ' * number + '*')
+
+# print('*******')
+# print()
+# print()
 
 
 
@@ -49,21 +47,21 @@ print()
 
 
 
-print('Ispis trokuta varijabilne visine pomocu for petlje')
-
-height = int(input('Unesite visinu trokuta: '))
-
-print('*')
-for number in range(height - 2):
-    # print('*' + ' ' * number + '*')
-    print(f'*{" " * number}*')
-
-# print('*' + '*' * (height - 2) + '*')
-print(f'{"*" * height}')
-print()
-print()
 
 
+# print('Ispis trokuta varijabilne visine pomocu for petlje')
+
+# height = int(input('Unesite visinu trokuta: '))
+
+# print('*')
+# for number in range(height - 2):
+#     # print('*' + ' ' * number + '*')
+#     print(f'*{" " * number}*')
+
+# # print('*' + '*' * (height - 2) + '*')
+# print(f'{"*" * height}')
+# print()
+# print()
 
 
 
@@ -77,16 +75,42 @@ print()
 
 
 
-print('Ispis trokuta varijabilne visine pomocu for petlje')
-print('uz izbor znaka za iscrtavanje linija strnica trokuta')
 
-height = int(input('Unesite visinu trokuta: '))
-symbol = input('Unesite znak za iscrtavanje stranica trokuta: ')
 
-print(symbol)
-for number in range(height - 2):
-    print(f'{symbol}{" " * number}{symbol}')
+# print('Ispis trokuta varijabilne visine pomocu for petlje')
+# print('uz izbor znaka za iscrtavanje linija strnica trokuta')
 
-print(f'{symbol * height}')
-print()
-print()
+# height = int(input('Unesite visinu trokuta: '))
+# symbol = input('Unesite znak za iscrtavanje stranica trokuta: ')
+
+# print(symbol)
+# for number in range(height - 2):
+#     print(f'{symbol}{" " * number}{symbol}')
+
+# print(f'{symbol * height}')
+# print()
+# print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Pitanje 
+# Vladimir Stipetić: zašto moram staviti u zagradu x + 0?
+
+z = input('znak za ispis stranica trokuta: ')
+x = int(input('visina stranice trokuta: '))
+
+print(z)
+for number in range(x - 2):
+    print(z + ' ' * number + z)
+print(z * x + 0) # ne radi jer nije moguce povezivati (+)  niti oduzimati (-) str i int tipove podataka
+print(z * x + '0')
